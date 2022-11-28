0a29b69e753ba3d09130cea3644422cf83faa26e
Konrad Hajduga <konrad.hajduga@tratif.com>
2022-11-28 16:26:21 +0100

    Removed redundant code

--- src/main/java/swagger/SpecificationArgResolverSpringdocOperationCustomizer.java
+++ src/main/java/swagger/SpecificationArgResolverSpringdocOperationCustomizer.java
@@ -12,11 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.method.HandlerMethod;
 
 import java.lang.annotation.Annotation;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import static java.util.Arrays.stream;
 import static java.util.Collections.emptyList;
 import static java.util.Objects.isNull;
 import static java.util.Optional.ofNullable;
@@ -37,27 +36,14 @@ public class SpecificationArgResolverSpringdocOperationCustomizer implements Ope
 			for (Class<?> cls : methodParameter.getParameterType().getInterfaces()) {
 				if (cls == Specification.class) {
 
-					// Obtain the list of joins of the specification
-					Map<String, String> joinMap = new HashMap<>();
-					for (Annotation anno : methodParameter.getParameterType().getAnnotations()) {
-						if (anno.annotationType() == Join.class) {
-							joinMap.put(((Join) anno).alias(), ((Join) anno).path());
-						} else if (anno.annotationType() == RepeatedJoin.class) {
-							for (Join join : ((RepeatedJoin) anno).value()) {
-								joinMap.put(join.alias(), join.path());
-							}
-						}
-					}
-
-					// Create doc param from annotations
 					for (Annotation anno : methodParameter.getParameterType().getAnnotations()) {
 						if (anno.annotationType() == Spec.class) {
-							createParameter((Spec) anno, joinMap, requiredParameters).forEach(operation::addParametersItem);
+							createParameter((Spec) anno, requiredParameters).forEach(operation::addParametersItem);
 						}
 
 						if (annotationContainsNestedSpecification(anno)) {
 							for (Spec spec : ((Or) anno).value()) {
-								createParameter(spec, joinMap, requiredParameters).forEach(operation::addParametersItem);
+								createParameter(spec, requiredParameters).forEach(operation::addParametersItem);
 							}
 						}
 					}
@@ -66,6 +52,23 @@ public class SpecificationArgResolverSpringdocOperationCustomizer implements Ope
 		}
 
 		return operation;
+	}
+
+	private List<Parameter> createParameter(Spec spec, List<String> requiredParameters) {
+		Schema<String> defaultParamSchema = new Schema<>();
+		defaultParamSchema.setType("string");
+
+		return stream(spec.params())
+				.map(parameterName -> {
+					Parameter specificationParam = new Parameter();
+					specificationParam.setName(parameterName);
+					specificationParam.setRequired(requiredParameters.contains(spec.path()));
+					specificationParam.setIn("query");
+					specificationParam.setSchema(defaultParamSchema);
+
+					return specificationParam;
+				})
+				.collect(Collectors.toList());
 	}
 
 	private List<String> extractRequiredParametersFromHandlerMethod(HandlerMethod handlerMethod) {
@@ -82,32 +85,4 @@ public class SpecificationArgResolverSpringdocOperationCustomizer implements Ope
 				annotation.annotationType() == Or.class;
 	}
 
-	private List<Parameter> createParameter(Spec spec, Map<String, String> joinMap, List<String> requiredParameters) {
-		List<Parameter> result = new ArrayList<>();
-
-		Schema<String> paramSchema = new Schema<>();
-		paramSchema.setType("string");
-		paramSchema.setDefault(null);
-
-		for (String paramName : spec.params()) {
-
-			// Get the alias if any join exists, perform while loop for nested loops
-			String path = spec.path();
-			while (path.contains(".")) {
-				String[] splitStr = path.split("\\.");
-				path = joinMap.get(splitStr[0]) + ":" + splitStr[1];
-			}
-
-			Parameter specificationParam = new Parameter();
-			specificationParam.setName(paramName);
-			specificationParam.setDescription(
-					String.format("Will search for parameter %s using matching method: %s", path, spec.spec().getSimpleName()));
-			specificationParam.setRequired(requiredParameters.contains(path)); // if request param annotation contains specified required parameters
-			specificationParam.setIn("query");
-			specificationParam.setSchema(paramSchema);
-
-			result.add(specificationParam);
-		}
-		return result;
-	}
 }
